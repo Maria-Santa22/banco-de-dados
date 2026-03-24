f8147ddd12e2fca3bb498b20b3672f0c3e571372
Maria <msgalaxy.hpts@gmail.com>
2026-03-23 20:40:43 -0500

    código simples, precisa ser terminado

--- empresa.sql
+++ empresa.sql
@@ -1,13 +1,14 @@
 CREATE DATABASE empresa_total;
- 
-USE total; 
-CREATE TABLE  IF NOT EXISTS usuarios(
+
+USE empresa_total;
+
+CREATE TABLE IF NOT EXISTS usuarios(
 	id_user INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     telefone VARCHAR(25)
 );
-USE total;
+
 CREATE TABLE IF NOT EXISTS servicos(
 	id INT PRIMARY KEY AUTO_INCREMENT,
     id_user INT,
@@ -16,27 +17,23 @@ CREATE TABLE IF NOT EXISTS servicos(
     FOREIGN KEY(id_user) REFERENCES usuarios(id_user)
 );
 
-USE total;
 CREATE TABLE IF NOT EXISTS avaliacoes(
- id INT PRIMARY KEY AUTO_INCREMENT,
- id_user INT, 
- nota DECIMAL(3,1),
- comentario TEXT,
- data_avaliacao DATETIME
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	id_user INT, 
+	nota DECIMAL(3,1),
+	comentario TEXT,
+	data_avaliacao DATETIME,
+    FOREIGN KEY(id_user) REFERENCES usuarios(id_user)
 );
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES ('Maria de Melo', 'mariamelo@gmail.com', '(68) 9934-1029');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES ('Luciana Souza', 'souzalu@gmail.com', '(68) 9349-2109');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Paulo Pinheiro', 'paulogpt@gmail.com', '(68) 9493-0921');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Aline Barros', 'barros@gmail.com', '(68) 9987-2901');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Luan Pereira', 'pereira@gmail.com', '(69) 9872-2215');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Vitória Nunes', 'nnunes@gmail.com', '(11) 9830-3203');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Tim Maia', 'maiatim@gmail.com', '(11) 9306-2810');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Melissa Lima', 'mel@gmail.com', '(11) 9509-2620');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Jorge Cardoso', 'jorginho@gmail.com', '(19) 9920-7966');
- INSERT INTO usuarios.empresa_total(nome, email, telefone) VALUES('Daniel da Costa', 'costinha@gmail.com', '(18) 9425-9567');
- 
 
- 
-
-
-
+INSERT INTO usuarios(nome, email, telefone) VALUES 
+('Maria de Melo', 'mariamelo@gmail.com', '(68) 9934-1029'),
+('Luciana Souza', 'souzalu@gmail.com', '(68) 9349-2109'),
+('Paulo Pinheiro', 'paulogpt@gmail.com', '(68) 9493-0921'),
+('Aline Barros', 'barros@gmail.com', '(68) 9987-2901'),
+('Luan Pereira', 'pereira@gmail.com', '(69) 9872-2215'),
+('Vitória Nunes', 'nnunes@gmail.com', '(11) 9830-3203'),
+('Tim Maia', 'maiatim@gmail.com', '(11) 9306-2810'),
+('Melissa Lima', 'mel@gmail.com', '(11) 9509-2620'),
+('Jorge Cardoso', 'jorginho@gmail.com', '(19) 9920-7966'),
+('Daniel da Costa', 'costinha@gmail.com', '(18) 9425-9567');
